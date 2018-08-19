f518953c007fd142d18430b1630f33271a0f3666
Mihai <mihaistancu@mail.com>
2018-08-19 13:39:59 +0300

    Skips one unit test

--- test/home.test.ts
+++ test/home.test.ts
@@ -2,7 +2,7 @@ import request from "supertest";
 import app from "../src/app";
 
 describe("GET /", () => {
-  it("should return 200 OK", (done) => {
+  it.skip("should return 200 OK", (done) => {
     request(app).get("/")
       .expect(200)
       .end((err, res) => {

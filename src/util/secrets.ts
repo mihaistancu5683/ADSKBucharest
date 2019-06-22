893da01af54b593c1885731f38f1ebcffa303cac
Mihai <mihaistancu@mail.com>
2019-06-22 21:49:10 +0300

    Adds checks for setting environment variables

--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -10,14 +10,18 @@ if (fs.existsSync(".env")) {
     dotenv.config({ path: ".env.example" });  // you can delete this after you create your own .env file!
 }
 export const ENVIRONMENT = process.env.NODE_ENV;
-const prod = ENVIRONMENT === "production"; // Anything else is treated as 'dev'
 
 export const SESSION_SECRET = process.env["SESSION_SECRET"];
 export const ADSK_CLIENT_ID = process.env["ADSK_CLIENT_ID"];
 export const ADSK_CLIENT_SECRET = process.env["ADSK_CLIENT_SECRET"];
 export const ADSK_CALLBACK_URI = process.env["ADSK_CALLBACK_URI"];
 
-export const MONGODB_URI = prod ? process.env["MONGOLAB_URI"] : process.env["MONGODB_URI_LOCAL"];
+export const MONGODB_URI = process.env["MONGODB_URI"];
+
+if (!ENVIRONMENT) {
+    logger.error("No node.js environment. Set NODE_ENV environment variable.");
+    process.exit(1);
+}
 
 if (!SESSION_SECRET) {
     logger.error("No client secret. Set SESSION_SECRET environment variable.");
@@ -26,5 +30,20 @@ if (!SESSION_SECRET) {
 
 if (!MONGODB_URI) {
     logger.error("No mongo connection string. Set MONGODB_URI environment variable.");
+    process.exit(1);
+}
+
+if (!ADSK_CLIENT_ID) {
+    logger.error("No client ID. Set ADSK_CLIENT_ID environment variable.");
+    process.exit(1);
+}
+
+if (!ADSK_CLIENT_SECRET) {
+    logger.error("No client secret. Set ADSK_CLIENT_SECRET environment variable.");
+    process.exit(1);
+}
+
+if (!ADSK_CALLBACK_URI) {
+    logger.error("No callback URI. Set ADSK_CALLBACK_URI environment variable.");
     process.exit(1);
 }
